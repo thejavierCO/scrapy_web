9b97dea7c727caacbbc396d6594aa6e7e7b63500
javier cruz obispo <javier.cruz.odispo@hotmail.com>
2022-09-17 18:06:30 -0500

    save in file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 require('dotenv').config()
+const fs = require('fs')
+const path = require('path')
 const SADM = require('./page/sadm')
 
 async function Main() {
@@ -6,6 +8,6 @@ async function Main() {
   await test.login(process.env.SADM_EMAIL, process.env.SADM_PASS)
   let data = await test.getTableService()
   test.Exit()
-  console.log(data)
+  fs.writeFileSync(path.join(__dirname, 'test.json'), JSON.stringify(data))
 }
 Main()
