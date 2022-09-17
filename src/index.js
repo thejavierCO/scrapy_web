39f0427e08a97e38cbe1c67dcdb1079a4337e2ab
javier cruz obispo <javier.cruz.odispo@hotmail.com>
2022-09-17 00:27:36 -0500

    testing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 require('dotenv').config()
 const puppeteer = require('puppeteer')
 async function run() {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({ headless: false })
   const page = await browser.newPage()
   await page.goto('https://ayd.sadm.gob.mx/eAyd/Login.jsp')
-
   await page.type('form > input[name=email].form-control', process.env.EMAIL)
   await page.type('form > input[name=password].form-control', process.env.PASS)
   await page.click('form > input[type=button]')
@@ -16,12 +15,23 @@ async function run() {
     )
       .filter((e) => e.id == '')
       .map((e) =>
-        Array.from(e.querySelectorAll('td')).filter(
-          (e) => e.innerText != '' && e.childNodes.length == 1,
-        ),
+        Array.from(e.querySelectorAll('td'))
+          .filter((e) => e.innerText != '' && e.childNodes.length == 1)
+          .map((e) => e.innerText),
       )
-    console.log(tags)
   })
 }
 
 run()
+
+/**
+ * Array.from(
+      document.querySelector('table#tabla_servicios1').querySelectorAll('tr'),
+    )
+      .filter((e) => e.id == '')
+      .map((e) =>
+        Array.from(e.querySelectorAll('td'))
+          .filter((e) => e.querySelectorAll("input").length ==1)
+          .map((e) => e),
+      )
+ */
