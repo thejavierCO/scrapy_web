a8a829a6a552d241b863ccd3a22b255ddcdf3b78
javier cruz obispo <javier.cruz.odispo@hotmail.com>
2022-09-17 17:28:48 -0500

    update page system

--- src/js/page.js
+++ src/js/page.js
@@ -4,12 +4,13 @@ class Page {
   constructor(url, view = false) {
     this.url = url
     this.Navegator = Navegator(view)
+    this.page = undefined
   }
   async start() {
     const N = await this.Navegator
     const page = await N.newPage()
     await page.goto(this.url)
-    return page
+    this.page = page
   }
   async Exit() {
     return await (await this.Navegator).close()
