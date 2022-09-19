3f2ca369b87e386f4113f70dedba0707b8be0f21
javier cruz obispo <javier.cruz.odispo@hotmail.com>
2022-09-18 21:48:20 -0500

    modify testing service page

--- src/page/naturagy.js
+++ src/page/naturagy.js
@@ -1,1 +1,24 @@
-class Naturagy {}
+const Page = require('../js/page')
+
+class Naturagy extends Page {
+  constructor(view) {
+    super(
+      'https://apolo.force.com/NaturgyContigo/s/login/?language=es_MX',
+      view,
+    )
+  }
+  async login(user, password) {
+    const page = await this.start()
+    await page.waitForSelector('form.cLoginForm')
+    await page.type('form.cLoginForm > input#input-11.slds-input', user)
+    await page.type('form.cLoginForm > input#input-13.slds-input', password)
+    await page.click(
+      'form.cLoginform > button.slds-button.slds-button_brand.slds-form-element__control.slds-m-top--medium.slds-button_stretch.btn.radiousborder',
+    )
+  }
+  async getDataService() {
+    console.log('code')
+  }
+}
+
+module.exports = Naturagy
