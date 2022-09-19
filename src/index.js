bbefea1fd3ad5f84c6a1f8b3a235e6549d38d9cc
javier cruz obispo <javier.cruz.odispo@hotmail.com>
2022-09-18 21:21:36 -0500

    add testing cfe

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ require('dotenv').config()
 const fs = require('fs')
 const path = require('path')
 const SADM = require('./page/sadm')
+const CFE = require('./page/cfe')
 
 async function Main() {
-  let test = new SADM()
-  await test.login(process.env.SADM_EMAIL, process.env.SADM_PASS)
+  let test = new CFE()
+  await test.login(process.env.CFE_EMAIL, process.env.CFE_PASS)
   let data = await test.getTableService()
   test.Exit()
-  fs.writeFileSync(path.join(__dirname, 'test.json'), JSON.stringify(data))
+  console.log(data)
+  // fs.writeFileSync(path.join(__dirname, 'test.json'), JSON.stringify(data))
 }
 Main()
