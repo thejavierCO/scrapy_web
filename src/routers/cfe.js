09e3534019198aa6cf7b9c04813bbee95f4eaf8b
javier cruz obispo <javier.cruz.odispo@hotmail.com>
2022-09-26 19:31:45 -0500

    see errors

--- src/routers/cfe.js
+++ src/routers/cfe.js
@@ -3,12 +3,16 @@ const CFE = require('../page/cfe')
 const router = require('express').Router()
 
 router.get('/', async (req, res, next) => {
-  let data = {}
-  let luz = new CFE()
-  await luz.login(config.CFE.user, config.CFE.pass)
-  data = await luz.getTableService()
-  luz.Exit()
-  res.json(data)
+  try {
+    let data = {}
+    let luz = new CFE()
+    await luz.login(config.CFE.user, config.CFE.pass)
+    data = await luz.getTableService()
+    luz.Exit()
+    res.json(data)
+  } catch (err) {
+    res.json({ err: err.message })
+  }
 })
 
 module.exports = router
--- src/routers/sadm.js
+++ src/routers/sadm.js
@@ -3,12 +3,16 @@ const SADM = require('../page/sadm')
 const router = require('express').Router()
 
 router.get('/', async (req, res, next) => {
-  let data = {}
-  let wheater = new SADM()
-  await wheater.login(config.SADM.user, config.SADM.pass)
-  data = await wheater.getTableService()
-  wheater.Exit()
-  res.json(data)
+  try {
+    let data = {}
+    let wheater = new SADM()
+    await wheater.login(config.SADM.user, config.SADM.pass)
+    data = await wheater.getTableService()
+    wheater.Exit()
+    res.json(data)
+  } catch (err) {
+    res.json({ err: err.message })
+  }
 })
 
 module.exports = router
