af2f85b5521dcbf8ad8ad1eb5abf9d491b3c8e78
javier cruz obispo <javier.cruz.odispo@hotmail.com>
2022-09-28 20:13:57 -0500

    upload supabse

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 const config = require('./config')
 const CFE = require('./scrapyweb/page/cfe')
+const supabse = require('./supabase')
 
 async function Main() {
-  let wheater = new CFE()
+  let wheater = new CFE(true)
   await wheater.login(config.CFE.user, config.CFE.pass)
   let data = await wheater.getTableService().then((e) =>
-    e.map(({ street, data }) => ({
-      id: parseInt(data['Número de servicio'].replace(/ /g, '')),
-      street,
-      date: data['Fecha límite de pago'],
-      price:
-        data['Estado del recibo'] == 'PAGADO'
-          ? parseInt(data['Monto a pagar por el período'].slice(1))
-          : 0,
-    })),
+    e
+      .map(({ street, data }) => ({
+        id: parseInt(data['Número de servicio'].replace(/ /g, '')),
+        street,
+        date: data['Fecha límite de pago'],
+        price:
+          data['Estado del recibo'] == 'PAGADO'
+            ? parseInt(data['Monto a pagar por el período'].slice(1))
+            : 0,
+      }))
+      .map(async (e) => await supabse.from('luz').insert(e)),
   )
   wheater.Exit()
   return data
